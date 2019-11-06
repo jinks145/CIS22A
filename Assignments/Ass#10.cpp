82910d3ec4c96b7636dcb4fdd4add1d7e3a0064a
Brad <jeongjooho1995@gmail.com>
2019-11-06 10:13:10 -0800

    finished analysis

--- Assignments/Ass#10.cpp
+++ Assignments/Ass#10.cpp
@@ -4,11 +4,130 @@ Compiler : CodeBlocks
 Operating System : Win32
 Name: Brad
 */
+
 #include <iostream>
+#include <cstring>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
 #include <string>
+#include <cstdlib>
 using namespace std;
 
+struct data{
+    string date;
+    double prices[5];
+    int volume;
+};
+
+struct results{
+    string startDate;
+    float c_price,gain, pct_gain, m_close, Avg_close; 
+    int marketDays, u_dates, d_dates;
+};
+
+void load(ifstream &ifile, data * d);
+void analysis(data data[], results * result, int d_size);
+float maxClose(double close[], int size);
+float avgCLose(double close[], int size);
 int main(){
+    ifstream ifile("cat.csv");
+    ofstream ofile("report.csv");
+    data data[1001];
+    results result[8];
+    
+
+    for(int i = 1000; i > -1; i--){
+        load(ifile, &data[i]);
+    }
+
+    analysis(data, &result[0], 5);
+
+    cout << result[0].marketDays << "  " << result[0].startDate << "  " << result[0].c_price << "  " << result[0].u_dates
+    << "  " << result[0].d_dates << "  " << result[0].gain << "  " << result[0].pct_gain << "  " << result[0].m_close << "  " << result[0].Avg_close; 
 
 }
 
+
+void load(ifstream &ifile , data * d){
+    
+    string line, data[7];//line for 
+    
+    if(ifile.is_open()){
+        getline(ifile, line);
+
+        if(line == "Date,Open,High,Low,Close,Adj Close,Volume") return;// omits the line
+
+        stringstream ss(line);
+
+            
+
+            for(int i = 0; i < 7; i++){
+                getline(ss , data[i], ',');
+            }
+
+        d-> date = data[0];
+
+        for(int i = 1; i <= sizeof(d-> prices)/sizeof(double); i++){
+            
+            d -> prices[i - 1] = atof(data[i].c_str());
+
+        }
+
+        d->volume = atoi(data[6].c_str());
+
+    }
+    else{
+        cout << "file open failure" << endl;
+        exit(1);
+    }
+}
+
+float maxClose(double close[], int size){
+    float max = 0.0;
+    for(int i = 0; i < size; i++){
+        if(close[i] > max)
+        max = close[i];
+    }
+
+    return max;
+}
+
+float avgClose(double close[], int size){
+    float avg = 0.0;
+
+    for(int i = 0; i < size; i++){
+        avg += close[i];
+    }
+
+    return avg / size;
+}
+
+
+void analysis(data d[], results * result, int marketDays){
+    double close[marketDays];
+    int down = 0, up = 0;
+    data current = d[0];
+    result->marketDays = marketDays;
+    
+    for(int i = 0; i < marketDays; i++){
+        if(d[i].prices[0] > d[i].prices[3]){
+            down++;
+        }
+
+        else if(d[i].prices[0] < d[i].prices[3]){
+            up++;
+        }
+        close[i] = d[i].prices[3];
+    }
+    result ->u_dates = up;
+    result ->d_dates = down;
+    result -> c_price = d[marketDays - 1].prices[3];
+    result -> startDate = d[marketDays - 1].date;
+    result -> gain = current.prices[3] - d[marketDays - 1].prices[3];
+
+    result -> pct_gain = result -> gain / d[marketDays - 1].prices[3];
+
+    result -> m_close = maxClose(close, marketDays);
+    result -> Avg_close = avgClose(close, marketDays);
+}
--- Assignments/Ass7.cpp
+++ Assignments/Ass7.cpp
@@ -7,28 +7,30 @@
 using namespace std;
 
 /*
-Assignment#
+Assignment#7
 Compiler : CodeBlocks
 Operating System : Win32
 Name: Brad
+Note: Please note that load, dump and report functions perform file open checks.
 */
 
 //implemented functions
-void load(ifstream &ifile , int * arr, int size);
-void dump(ofstream &ofile, int * arr, int * scores , int size, int size2);
-void report(ofstream &ofile, float * arr);
+void load(ifstream &ifile , int * arr, int start, int end);// loads file
+void dump(int ID, ofstream &ofile, int * arr, int * scores , int size);//dumps data that was calculated from the loaded data
+void report(ofstream &ofile, float * stats, int * ID);// writes them to the file
 int percentage(int total);// finds percentage from the total calculated
 string grader(int percentage);// grade based on percentage
-int total(int * arr, int size);// calculates the sum of elements in an array
+int total(int * arr, int start, int end);// calculates the sum of elements in an array
 int max(int * arr, int size);// finds the largest number in the array
 int min(int * arr, int size);// finds the smallest number in the array
 const int MAX = 400;// the highest score possible as a constant
 
 int main () {
     
-    int ID, assignments[11] = {}, scores[6]= {};
-    float stats[5] ={};
-     // scores[0] == Ass, scores[1] = Mid, scores[2] = Fin, scores[3] = LEx, scores[4] = total, scores[5] = PCT;
+    int ID[3] = {},  assignments[11] = {}, scores[6]= {};//ID[] stores ids
+    // scores[0] == Ass, scores[1] = Mid, scores[2] = Fin, scores[3] = LEx, scores[4] = total, scores[5] = PCT;
+    float stats[7] ={0,0,0,0,0,400.0,100.0};
+    // stats[0]= student number, stats[1]=  avg points, stats[2] = avg total, stats[3]=  h_points, stats[4]=  h_pct , stats[5]=  l_points, stats[6]=  l_pct}
     string grade;//actual grade
     ifstream ifile("ass7data.txt");//data source
     ofstream ofile("ass7out.txt");// stored in ass6out.txt
@@ -40,8 +42,45 @@ int main () {
         cerr <<  "file open failure!";
         exit(1);
     }else{
+        ofile << "Student   -----   Assignment Grades  -----  Ass  Mid  Fin LEx Total  Pct Gr" << endl;
+        ofile << "--------  -- -- -- -- -- -- -- -- -- -- --  ---  ---  --- --- -----  --- --" << endl;
+
+        while(!ifile.eof()){
+            ifile >> ID[0];
+            if(ifile.eof()) break;
 
 
+        load(ifile, assignments, 0 , 10);
+
+        scores[0] = total(assignments, 0 ,10) - min(assignments, 11);
+        load(ifile, scores, 1, 3);
+        scores[4] = total(scores, 0, 3);        
+        scores[5] = percentage(scores[4]);
+
+        dump(ID[0], ofile, assignments, scores, 11);
+
+        if(stats[3] < scores[4]){
+                ID[1] = ID[0];
+                stats[3] = scores[4];
+                stats[4] = scores[5];
+        }
+
+        if(stats[5] > scores[4]){
+                ID[2] = ID[0];
+                stats[5] = scores[4];
+                stats[6] = scores[5];
+        }
+
+        
+
+
+        stats[1] += scores[4];
+        stats[2] += scores[5];
+        stats[0]++;
+        
+        }
+
+        report(ofile, stats, ID);
 
         
         ifile.close();
@@ -52,19 +91,19 @@ int main () {
 
 
 
-int total(int * arr, int size){
+int total(int * arr, int start, int end){
     int total = 0;
 
-    for(int i = 0; i < size; i++){
+    for(int i = start; i <= end; i++){
         total += arr[i];
     }
 
     return total;
 }
 
-void load(ifstream &ifile , int * arr, int size){
+void load(ifstream &ifile , int * arr, int start, int end){
     if(ifile.is_open()){
-        for(int i = 0; i < size; i++){
+        for(int i = start; i <= end; i++){
             ifile >> arr[i];
 
             if(ifile.eof()){
@@ -79,15 +118,28 @@ void load(ifstream &ifile , int * arr, int size){
     }
 }
 
-void dump(ofstream &ofile, int * arr, int * scores , int size, int size2){
+void dump(int ID, ofstream &ofile, int * arr, int * scores , int size){
     
     
     if(ofile.is_open()){
-        ofile << "Student   -----   Assignment Grades  -----  Ass  Mid  Fin LEx Total  Pct Gr" << endl;
-        ofile << "--------  -- -- -- -- -- -- -- -- -- -- --  ---  ---  --- --- -----  --- --" << endl;
+        
 
-        ofile << setfill('0') << setw(8)  << arr[0] << setfill(' ') << " ";// pads 8 digit id with leading 0s && resets them when finished
+        ofile << setfill('0') << right << setw(8)  << ID << setfill(' ') << " ";// pads 8 digit id with leading 0s && resets them when finished
 
+
+        for(int i = 0; i  < size; i++){
+            ofile << setw(3) << arr[i];
+        }
+        
+        
+
+        for(int j = 0; j < 3; j++){
+            ofile << setw(5) << scores[j];
+        }
+
+        ofile << setw(4) << scores[3];
+
+        ofile << setw(6) << scores[4] << setw(5) << scores[5] << left << setw(3) <<" " + grader(scores[5]) << endl;
 
     }
     else{
@@ -96,19 +148,18 @@ void dump(ofstream &ofile, int * arr, int * scores , int size, int size2){
     }
 
 }
-void report(ofstream &ofile, float * arr){
+void report(ofstream &ofile, float * stats, int * ID){
     
     if(ofile.is_open()){
 
-            
-            ofile <<  "Number of students = " << arr[0] << endl;
-            ofile <<  "The average total points = " << arr[1] << endl;
-            ofile <<  "The average percent total = " << arr[2] << endl;
-            ofile <<  "Highest Grade: " << arr[3] << endl;
-            ofile << "Lowest Grade: " << arr[4] << endl;
+            ofile <<  "\n\nNumber of students = " << stats[0] << endl;
+            ofile <<  "The average total points = " << fixed << setprecision(1) << stats[1] / stats[0] << endl;
+            ofile <<  "The average percent total = " << fixed << setprecision(1) << stats[2] / stats[0] << "%" << endl;
+            ofile <<  "Highest Grade: " <<" Id=" << setw(8) << ID[1] <<"  Points="<< static_cast<int>(stats[3]) <<"  Percent=" << stats[4] << "%" << endl;
+            ofile << "Lowest Grade: " << " Id=" << setw(8) << ID[2] << "  Points=" << static_cast<int>(stats[5]) << "  Percent=" << stats[6] <<"%" << endl;
     }
     else{
-        cout << "file open failure" << endl;
+        cerr << "file open failure" << endl;
         exit(1);
     }
 }
