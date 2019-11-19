21f315be1eedaca346381f72a929771f4c7638ab
Brad <jeongjooho1995@gmail.com>
2019-11-18 20:03:02 -0800

    finished selection sort

--- Exercises/Ftests/main.cpp
+++ Exercises/Ftests/main.cpp
@@ -12,25 +12,50 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 using namespace std;
 
+int partition(int arr[],int lo, int hi){
+    int pivot = arr[hi];
+    int tmp;
+    int i = lo - 1;
 
-int load(ifstream &ifile, int * arr, int size){
-   
-    for(int i = 0; i < size; i++){
-         ifile >> arr[i];
-         ifile.ignore(44, '\n');
+    for(int j = lo; j <= hi - 1; j++){
+        if(arr[j] < pivot){
+            i++;
+            tmp = arr[j];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        }
     }
+
+            tmp = arr[i+1];
+            arr[i+1] = arr[hi];
+            arr[hi] = tmp;
+
+    return i+1;
 }
+
+void qsort(int arr[], int lo, int hi){
+   
+   if(lo < hi){
+        int pi = partition(arr, lo, hi);
+
+        qsort(arr, lo, pi - 1);
+       
+        qsort(arr, pi + 1, hi);
+   }
+
+}
+
 
 
 int main()
 {
-    ifstream ifile("ass6data.txt");
-    int IDs[50];
+    //ifstream ifile("ass6data.txt");
+    int IDs[7] = {10, 80, 30, 90, 40, 50, 70};
 
-    load( ifile,IDs, 50);
+    qsort(IDs, 0, 6);
 
-    for(int i = 0; i < 50; i++){
-        cout << IDs[i] << endl; 
+    for(int i = 0; i < 7; i++){
+        cout << IDs[i] << ", ";
     }
 }
 
